d9b4d4a10f9e37c847445a38d69e7f268314f7d4
Optimum <pixelmaltd@gmail.com>
2022-09-14 22:21:12 +0100

    changes

--- ThievingModule.cs
+++ ThievingModule.cs
@@ -1,10 +1,12 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using AresFramework.Plugin.Loaders;
 using AresFramework.Plugin.Module;
 using AresFramework.Plugins.Thieving.Pickpocketing;
 using JetBrains.Annotations;
 using NLog;
+using Polly;
 
 namespace AresFramework.Plugins.Thieving;
 
@@ -24,9 +26,19 @@ public class ThievingModule : IPluginModule
         PluginLoader.InitializeTypes<IPickpocketNpc>(typeof(IPickpocketNpc), (pickpocketNpc, name) =>
         {
             PickpocketNpcs.MapPickpocketNpc(pickpocketNpc);
-        });
-        Log.Info($"Loaded {PickpocketNpcs.PickpocketingNpcs.Count} custom pickpocket npcs");
+        }); 
         
+        Log.Info($"Loaded {PickpocketNpcs.PickpocketingNpcs.Count} custom pickpocket npcs");
+        var retryPolicy = Policy.Handle<Exception>()
+            .WaitAndRetry(retryCount: 3, sleepDurationProvider: _ => TimeSpan.FromSeconds(1));
+        
+        var attempt = 0;
+        retryPolicy.Execute(() =>
+        {
+            Log.Info($"Attempt {++attempt}");
+
+            //throw new Exception();
+        });
         PickpocketNpcs.LoadNpcs();
         return Task.CompletedTask;
     }
